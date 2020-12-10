a4e29c2b9022acb5521ea0960caa3a928149e859
AlexanderBowers <7319abowers@gmail.com>
2020-12-09 16:48:48 -0800

    finished lab

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Alexander Bowers"
+    end
+
+    get '/hometown' do
+        "My hometown is Des Moines, WA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is The Imperial March"
+    end
+
 end
+
+
